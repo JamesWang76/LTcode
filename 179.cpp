627ff362d04689248a03bb4247b64436bd3ddb89
James Wang <jaja076076@gmail.com>
2019-06-21 01:00:11 +0800

    Medium 179
    Sort the "repeat number variable" and get the max string

--- 179.cpp
+++ 179.cpp
@@ -4,18 +4,25 @@
 #include <vector>
 using namespace std;
 string largestNumber(vector<int> &nums);
-string getStringVec(int maxDigit, vector<int> &nums);
-vector<pair<int, int>> dataInfo;
+string getStringVec(int maxDigit, vector<int> &nums, vector<pair<int, int>> dataInfo);
+
+struct comp {
+    template <typename T>
+    bool operator()(const T &l, const T &r) const {
+        return l > r;
+    }
+};
 
 int main() {
-    vector<int> x = {31, 30, 34499, 15, 97};
+    vector<int> x = {10, 2};
     // 31131
     // 31311
-    largestNumber(x);
+    cout << largestNumber(x);
 }
 
 string largestNumber(vector<int> &nums) {
     int maxDigit = 0;
+    vector<pair<int, int>> dataInfo;
     for (int i = 0; i < nums.size(); i++) {
         int tmpDigit = 0;
         int tmpValue = nums[i];
@@ -30,12 +37,12 @@ string largestNumber(vector<int> &nums) {
         if (tmpDigit > maxDigit)
             maxDigit = tmpDigit;
     }
-    return getStringVec(maxDigit, nums);
+    return getStringVec(maxDigit, nums, dataInfo);
 }
 
-string getStringVec(int maxDigit, vector<int> &nums) {
+string getStringVec(int maxDigit, vector<int> &nums, vector<pair<int, int>> dataInfo) {
     vector<pair<string, int>> stringVec;
-    string ans;
+    string ans = "";
     for (int i = 0; i < nums.size(); i++) {
         // cout<<dataInfo[i].first<<" "<<dataInfo[i].second<<"\n";
         string x = to_string(dataInfo[i].first);
@@ -52,6 +59,10 @@ string getStringVec(int maxDigit, vector<int> &nums) {
         tmpData.first = x;
         tmpData.second = dataInfo[i].first;
         stringVec.push_back(tmpData);
+    }
+    sort(stringVec.begin(), stringVec.end(), comp());
+    for (int i = 0; i < stringVec.size(); i++) {
+        ans += to_string(stringVec[i].second);
     }
     return ans;
 }
