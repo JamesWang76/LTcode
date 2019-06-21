8d73f3adb39753a53395d268be8fdcb27075d02f
James Wang <jaja076076@gmail.com>
2019-06-21 19:02:00 +0800

    Hard 135 submission

--- 135.cpp
+++ 135.cpp
@@ -4,25 +4,51 @@ using namespace std;
 
 int candy(vector<int> &ratings);
 
+/**
+ * The newest way:
+ *  - Go through the array from left to right and right to left.
+ *  - We can just consider the UP SLOPE case(ignore the down slope).
+ *  - From L ==> R:
+ *      - Store the (previous count++) if A[i] < A[i+1]
+ *  - From L <== R:
+ *      - ans+=max(LR[i], count) 
+ * 
+ * The original way:
+ * - From L ==> R:
+ *      - Store the (previous count++) if A[i] < A[i+1]
+ * - But it would neglect some specific situation.
+ * 
+ * TODO:
+ * - Fast but it takes too much memory usage.
+ */
+
 int main() {
-    vector<int> x = {1, 3, 2, 2, 1};
-    cout << candy(x);
+    // [29,51,87,87,72,12] = 12
+    // [1,3,2,2,1] = 7
+    // [1,3,4,5,2] = 11
+    vector<int> x = {29,51,87,87,72,12};
+    cout<<candy(x);
 }
 
 int candy(vector<int> &ratings) {
-    int arr[ratings.size()], ans = 0, minValue = 0;
-    memset(arr, 0, ratings.size() * sizeof(int));
-    for (int i = 1; i < ratings.size() - 1; i++) {
-        if (ratings[i] > ratings[i - 1] || ratings[i] > ratings[i + 1]) {
-            arr[i] = arr[i - 1] + 1;
-        } else if (ratings[i] < ratings[i - 1] || ratings[i] < ratings[i + 1]) {
-            arr[i] = arr[i - 1] - 1;
+    vector<int>LR;
+    LR.push_back(1);
+    int RLvalue = 1, ans=0;
+    for(int i=1;i<ratings.size();i++){
+        if(ratings[i] > ratings[i-1]){
+            LR.push_back(LR[i-1]+1);
+        }else{
+            LR.push_back(1);
         }
-        if (minValue > arr[i]) {
-            minValue = arr[i];
-        }
-        ans += arr[i];
     }
-    ans += (-minValue + 1) * ratings.size();
+    ans += max(RLvalue, LR[LR.size()-1]);
+    for(int i=ratings.size()-2;i>=0;i--){
+        if(ratings[i] > ratings[i+1]){
+            RLvalue++;
+        }else{
+            RLvalue = 1;
+        }
+        ans += max(LR[i], RLvalue);
+    }
     return ans;
 }
