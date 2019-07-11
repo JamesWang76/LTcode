05c0f97caf9c1277370bd9ba2a26480882c1569e
James Wang <jaja076076@gmail.com>
2019-07-11 16:15:42 +0800

    Medium 304 submission

--- 304.cpp
+++ 304.cpp
@@ -3,9 +3,23 @@
 #include <map>
 #include <vector>
 using namespace std;
+
+/**
+ * Find the partial original matrix, summing all the elements in this area.
+ * 
+ * - The better way
+ *      - Build a row prefix sum matrix
+ *      - notice that the case should be handle if the col1 equals to 0.
+ * 
+ * - The old way
+ *      - Using 2 for-loops to solve it
+ *      - BUG: time limit, it took too much time to run.
+ */
+
 void NumMatrix(vector<vector<int>>& matrix);
 int sumRegion(int row1, int col1, int row2, int col2);
-
+void printArr(vector<vector<int>>& matrix);
+int C1 = 0, R1 = 0, C2 = 0, R2 = 0;
 int main() {
     vector<vector<int>> data;
     int arr[5][5] = {
@@ -23,18 +37,35 @@ int main() {
     }
     NumMatrix(data);
 }
-vector<vector<int>> m;
+
+void printArr(vector<vector<int>>& matrix) {
+    for (int i = 0; i < matrix.size(); i++) {
+        for (int j = 0; j < matrix[0].size(); j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
+vector<vector<int>> rowPrefixSum;
 void NumMatrix(vector<vector<int>>& matrix) {
-    m = matrix;
-    sumRegion(1, 2, 2, 4);
+    rowPrefixSum = matrix;
+    for (int i = 0; i < matrix.size(); i++) {
+        for (int j = 1; j < matrix[0].size(); j++) {
+            rowPrefixSum[i][j] += rowPrefixSum[i][j - 1];
+        }
+    }
+    printArr(rowPrefixSum);
+    sumRegion(R1, C1, R2, C2);
 }
 
 int sumRegion(int row1, int col1, int row2, int col2) {
     int sum = 0;
-    for (int i = row1; i <= row2; i++) {
-        for (int j = col1; j <= col2; j++) {
-            sum += m[i][j];
-        }
+    for (int r = row1; r <= row2; r++) {
+        if (col1 > 0)
+            sum += (rowPrefixSum[r][col2] - rowPrefixSum[r][col1 - 1]);
+        else
+            sum += (rowPrefixSum[r][col2] - 0);
     }
+    cout << "sum: " << sum << "\n";
     return sum;
 }
