9d17b6f52cd566709e7c2e199ddb2ae68583a1c8
James Wang <jaja076076@gmail.com>
2019-07-14 12:37:02 +0800

    Have no idea to solve the Medium 15

--- 15.cpp
+++ 15.cpp
@@ -3,13 +3,28 @@
 #include <vector>
 using namespace std;
 vector<vector<int>> threeSum(vector<int>& nums);
-int binarySearch(vector<int> arr, int l, int r, int x, int skipIdx);
+int binarySearch(vector<int> arr, int l, int r, int x);
 void printDoubleArr(vector<vector<int>> x);
-void printArr(vector<int> x);
+int neighborSearch(vector<int> arr, int l, int r);
 
+void printArr(vector<int> x);
+/**
+ * The better way:
+ *      - Using difference between two value (right and left) 
+ *          -9,-7,-4,-3,-2,-1,2,5,7,8,11
+ *          -9 + 11 => mid: find -2
+ *          -9 + 8 => mid: find -2+(11-8) => 1 not found
+ *          -9 + 7 => mid: find 1 + (8-7) => find 2
+ *          -9 + 5 => mid: find 2 + (7-5) => 4 not found
+ *          STOP (UNTIL mid > right);
+ * The bad way:
+ *      - Using greedy algorithm
+ *      - ERROR: Time limit.
+ */
+  
 int main() {
     vector<int> nums;
-    int data[] = {-2,-2,0,2,4};
+    int data[] = {-1,-1,-1,0,0,0,1,2,1,1,2};
     for (int i = 0; i < (sizeof(data) / sizeof(data[0])); i++) {
         nums.push_back(data[i]);
     }
@@ -18,61 +33,54 @@ int main() {
 
 vector<vector<int>> threeSum(vector<int>& nums) {
     vector<vector<int>> ans;
-    vector<int> negativeArr, positiveArr, tmp;
-    int complementValue = 0;
-    int i, j, lastNegative=1, lastPositive=-1, zeroCtr=0;
-    for (i = 0; i < nums.size(); i++) {
-        if (nums[i] >= 0) {
-            if(nums[i]==0)zeroCtr++;
-            positiveArr.push_back(nums[i]);
-        } else {
-            negativeArr.push_back(nums[i]);
-        }
-    }
-    if(zeroCtr >=3){
-        vector<int> zeros;
-        for(i=0;i<3;i++)
-            zeros.push_back(0);
-        ans.push_back(zeros);
-    }
-    sort(positiveArr.begin(), positiveArr.end());
-    sort(negativeArr.begin(), negativeArr.end());
-    printArr(positiveArr);
-    printArr(negativeArr);
-    cout<<"==========\n";
-    for (i = 0; i < positiveArr.size(); i++) {
-        if(positiveArr[i] == lastPositive)continue;
-        else lastPositive = positiveArr[i];
-        for (j = 0; j < negativeArr.size(); j++) {
-            if(negativeArr[j] == lastNegative)continue;
-            else lastNegative = negativeArr[j];
-            if (positiveArr[i] <= (-1 * negativeArr[j])) {
-                complementValue = binarySearch(positiveArr, 0, positiveArr.size()-1, -1*(positiveArr[i]+negativeArr[j]), i);
-                if(complementValue && complementValue >= i){
-                    // cout<<positiveArr[i]<<"\n";
-                    // cout<<negativeArr[j]<<"\n";
-                    // cout<<complementValue<<"\n";
-                    tmp.push_back(negativeArr[j]);
-                    tmp.push_back(positiveArr[i]);
-                    tmp.push_back(positiveArr[complementValue]);
-                    ans.push_back(tmp);
-                    tmp.clear();
-                };
-            }else{
-                complementValue = binarySearch(negativeArr, 0, negativeArr.size()-1, -1*(positiveArr[i]+negativeArr[j]), j);
-                // cout<<"cv: "<<complementValue<<"\n";
-                if(complementValue && complementValue >= j){
-                    // cout<<positiveArr[i]<<"\n";
-                    // cout<<negativeArr[i]<<"\n";
-                    // cout<<complementValue<<"\n";
-                    tmp.push_back(negativeArr[j]);
-                    tmp.push_back(positiveArr[i]);
-                    tmp.push_back(negativeArr[complementValue]);
-                    ans.push_back(tmp);
-                    tmp.clear();
-                };
+    sort(nums.begin(), nums.end());
+
+    int i=0, j, lastPositiveValue = -1, lastNegativeValue = 1;
+    int complementLowerIdx = 0, tmpIdx, zeroCtr = 1;
+    bool zeroCalculated = false;
+    while(nums[i]<0){
+        if(lastNegativeValue == nums[i]){i++;continue;}
+        lastNegativeValue = nums[i];
+        complementLowerIdx = 0;
+        j=nums.size()-1;
+        while(nums[j]>=0 && j > complementLowerIdx){
+            if(!zeroCalculated && nums[j] == 0){
+                zeroCalculated = true;
+                if(nums [j-1] == nums[j-2] == 0){
+                    vector<int> zeros;
+                    for(i=0;i<3;i++)
+                        zeros.push_back(0);
+                    ans.push_back(zeros);
+                }
             }
+
+            if(lastPositiveValue == nums[j]){
+                if(nums[j]==0)zeroCtr++;
+                j--;
+                continue;
+            }
+            lastPositiveValue = nums[j];
+            if(complementLowerIdx == 0){
+                tmpIdx = binarySearch(nums, complementLowerIdx, j-1, -1*(nums[i]+nums[j]));
+                if(tmpIdx){
+                    complementLowerIdx = tmpIdx;
+                    vector<int> tmp;
+                    tmp.push_back(nums[i]);
+                    tmp.push_back(nums[j]);
+                    tmp.push_back(nums[complementLowerIdx]);
+                    ans.push_back(tmp);
+                    tmp.clear();
+                    neighborSearch(nums, complementLowerIdx, j-1);
+                }
+            }
+            
+            
+            j--;
         }
+        i++;
+    }
+    if(zeroCtr >= 3){
+        
     }
     return ans;
 }
@@ -93,15 +101,19 @@ void printDoubleArr(vector<vector<int>> x){
     
 }
 
-int binarySearch(vector<int> arr, int l, int r, int x, int skipIdx) {
+int binarySearch(vector<int> arr, int l, int r, int x) {
     if (r >= l) {
         int mid = l + (r - l) / 2;
-        if (arr[mid] == x && mid != skipIdx)
+        if (arr[mid] == x)
             return mid;
         if (arr[mid] > x)
-            return binarySearch(arr, l, mid - 1, x, skipIdx);
+            return binarySearch(arr, l, mid - 1, x);
 
-        return binarySearch(arr, mid + 1, r, x, skipIdx);
+        return binarySearch(arr, mid + 1, r, x);
     }
     return 0;
+}
+
+int neighborSearch(vector<int> arr, int l, int r) {
+    
 }
