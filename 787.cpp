5e2d8885a05418fb1bae189902e2411b764602b0
James Wang <jaja076076@gmail.com>
2019-08-08 11:43:37 +0800

    Medium 787
    BFS way

--- 787.cpp
+++ 787.cpp
@@ -1,32 +1,89 @@
 #include <iostream>
+#include <list>
 #include <queue>
 #include <vector>
+
 using namespace std;
 int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K);
-struct Edge {
-    int src, dest, weight;
+struct Edge{
+    int node, weight;
 };
 
-struct Graph {
-    int V, E;
-    struct Edge* edge;
+
+class Graph {
+    int V;  // No. of vertices
+    // Pointer to an array containing adjacency
+    // lists
+    list<Edge>* adj;
+
+   public:
+    int* step;
+    int* dist;
+    Graph(int V);  // Constructor
+
+    // function to add an edge to graph
+    void addEdge(int v, Edge w);
+
+    // prints BFS traversal from a given source s
+    void BFS(int s, int d, int K);
 };
 
-// 4
-// [[0,1,1],[0,2,5],[1,2,1],[2,3,1]]
-// 0
-// 3
-// 1
+Graph::Graph(int V) {
+    this->V = V;
+    adj = new list<Edge>[V];
+    this->step = new int[V];
+    memset(this->step, -1, V * sizeof(int));
+    this->dist = new int[V];
+    for(int i=0;i<V;i++){
+        this->dist[i] = INT_MAX;
+    }
+}
 
+void Graph::addEdge(int v, Edge w) {
+    adj[v].push_back(w);  // Add w to v’s list.
+}
+
+void Graph::BFS(int s, int d, int K) {
+    list<int> queue;
+    bool* visited = new bool[V];
+    for (int i = 0; i < V; i++)
+        visited[i] = false;
+
+    queue.push_back(s);
+    this->step[s] = 0;
+    this->dist[s] = 0;
+    list<Edge>::iterator i;
+
+    while (!queue.empty()) {
+        s = queue.front();
+        queue.pop_front();
+        for (i = adj[s].begin(); i != adj[s].end(); ++i) {
+            if (!visited[i->node]) {
+                if(adj[i->node].size() != 0)
+                    visited[i->node] = true;
+                this->step[i->node] = (this->step[s]) + 1;
+                if(this->dist[i->node] > this->dist[s] + i->weight){
+                    this->dist[i->node] = this->dist[s] + i->weight;
+                }
+                queue.push_back(i->node);
+            }
+        }
+        if(this->step[d] > K){
+            cout<<this->dist[d]<<"\n";
+            break;
+        }
+    }
+    cout<<this->dist[d]<<"\n";
+}
 
 int main() {
-    // int arr[3][3] = {{1, 0, 100}, {1, 2, 100}, {0, 2, 500}};
-    int arr[4][3] = {{0,1,1},{0,2,5},{1,2,1},{2,3,1}};
+    // int arr[3][3] = {{0, 1, 100}, {1, 2, 100}, {0, 2, 90}};
+    int arr[24][3] = {{3,4,4},{2,5,6},{4,7,10},{9,6,5},{7,4,4},{6,2,10},{6,8,6},{7,9,4},{1,5,4},{1,0,4},{9,7,3},{7,0,5},{6,5,8},{1,7,6},{4,0,9},{5,9,1},{8,7,3},{1,2,6},{4,1,5},{5,2,4},{1,9,1},{7,8,10},{0,4,2},{7,2,8}};
     vector<vector<int>> x;
     vector<int> tmp;
-    int n = 4, src = 0, dst = 3, K = 1;
+    int n = 10, src = 6, dst = 0, K = 7;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < 24; i++) {
         for (int j = 0; j < 3; j++) {
             tmp.push_back(arr[i][j]);
         }
@@ -36,45 +93,14 @@ int main() {
     findCheapestPrice(n, x, src, dst, K);
 }
 
-struct Graph* createGraph(int V, int E) {
-    struct Graph* graph = new Graph;
-    graph->V = V;
-    graph->E = E;
-    graph->edge = new Edge[E];
-    return graph;
-}
-
 int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
     int i, j;
-    int E = flights.size();
-    int dist[n];
-    for (i = 0; i < n; i++)
-        dist[i] = INT_MAX;
-    dist[src] = 0;
-    struct Graph* graph = createGraph(n, E);
+    Graph g(n);
     for (i = 0; i < flights.size(); i++) {
-        Edge tmpE;
-        tmpE.src = flights[i][0];
-        tmpE.dest = flights[i][1];
-        tmpE.weight = flights[i][2];
-        graph->edge[i] = tmpE;
-        if (tmpE.src == src)
-            dist[tmpE.dest] = tmpE.weight;
+        Edge e;
+        e.node = flights[i][1];
+        e.weight = flights[i][2];
+        g.addEdge(flights[i][0], e);
     }
-
-    for (i = 0; i < K; i++) {
-        for (j = 0; j < E; j++) {
-            int u = graph->edge[j].src;
-            int v = graph->edge[j].dest;
-            int weight = graph->edge[j].weight;
-            if (dist[u] != INT_MAX && dist[u] + weight < dist[v] && v == dst) {
-                dist[v] = dist[u] + weight;
-            }
-        }
-    }
-    if (dist[dst] == INT_MAX) {
-        cout << -1;
-    } else {
-        cout << dist[dst];
-    }
+    g.BFS(src, dst, K);
 }
